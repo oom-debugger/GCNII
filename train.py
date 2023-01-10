ade06fb7a8de316cdc3b3b413db25ce01bc19ed2
Mehrdad Khatir <mehrdadkhatir@gmail.com>
2023-01-10 12:25:48 -0500

    another way of adding layer

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from utils import *
 from model import *
+import statistics
 import uuid
 
 from riemannian.optimizer.radam import RiemannianAdam
@@ -51,93 +52,100 @@ adj = adj.to(device)
 checkpt_file = 'pretrained/'+uuid.uuid4().hex+'.pt'
 print(cudaid,checkpt_file)
 
-model = GCNII(nfeat=features.shape[1],
-                nlayers=args.layer,
-                nhidden=args.hidden,
-                nclass=int(labels.max()) + 1,
-                dropout=args.dropout,
-                lamda = args.lamda, 
-                alpha=args.alpha,
-                variant=args.variant,
-                curvature=args.c,
-                scale=args.scale,
-                ).to(device)
-
-if args.optimizer == 'adam':
-    optimiser_cls = optim.Adam
-elif args.optimizer == 'radam':
-    optimiser_cls = RiemannianAdam
-else:
-    raise NotImplementedError('%s optimiser has not been implemented!' % args.optimizer)
-
-optimizer = optimiser_cls([
-                        {'params':model.params1,'weight_decay':args.wd1},
-                        {'params':model.params2,'weight_decay':args.wd2},
-                        ],lr=args.lr)
-
-def train():
-    model.train()
-    optimizer.zero_grad()
-    output = model(features,adj)
-    acc_train = accuracy(output[idx_train], labels[idx_train].to(device))
-    loss_train = F.nll_loss(output[idx_train], labels[idx_train].to(device))
-    loss_train.backward()
-    optimizer.step()
-    return loss_train.item(),acc_train.item()
-
-
-def validate():
-    model.eval()
-    with torch.no_grad():
-        output = model(features,adj)
-        loss_val = F.nll_loss(output[idx_val], labels[idx_val].to(device))
-        acc_val = accuracy(output[idx_val], labels[idx_val].to(device))
-        return loss_val.item(),acc_val.item()
-
-def test():
-    model.load_state_dict(torch.load(checkpt_file))
-    model.eval()
-    with torch.no_grad():
-        output = model(features, adj)
-        loss_test = F.nll_loss(output[idx_test], labels[idx_test].to(device))
-        acc_test = accuracy(output[idx_test], labels[idx_test].to(device))
-        return loss_test.item(),acc_test.item()
+def stat_train():
+    model = GCNII(nfeat=features.shape[1],
+                    nlayers=args.layer,
+                    nhidden=args.hidden,
+                    nclass=int(labels.max()) + 1,
+                    dropout=args.dropout,
+                    lamda = args.lamda, 
+                    alpha=args.alpha,
+                    variant=args.variant,
+                    curvature=args.c,
+                    scale=args.scale,
+                    ).to(device)
     
-t_total = time.time()
-bad_counter = 0
-best = 999999999
-best_epoch = 0
-acc = 0
-for epoch in range(args.epochs):
-    loss_tra,acc_tra = train()
-    loss_val,acc_val = validate()
-    if(epoch+1)%1 == 0: 
-        print('Epoch:{:04d}'.format(epoch+1),
-            'train',
-            'loss:{:.3f}'.format(loss_tra),
-            'acc:{:.2f}'.format(acc_tra*100),
-            '| val',
-            'loss:{:.3f}'.format(loss_val),
-            'acc:{:.2f}'.format(acc_val*100))
-    if loss_val < best:
-        best = loss_val
-        best_epoch = epoch
-        acc = acc_val
-        torch.save(model.state_dict(), checkpt_file)
-        bad_counter = 0
+    if args.optimizer == 'adam':
+        optimiser_cls = optim.Adam
+    elif args.optimizer == 'radam':
+        optimiser_cls = RiemannianAdam
     else:
-        bad_counter += 1
-
-    if bad_counter == args.patience:
-        break
-
-if args.test:
-    acc = test()[1]
-
-print("Train cost: {:.4f}s".format(time.time() - t_total))
-print('Load {}th epoch'.format(best_epoch))
-print("Test" if args.test else "Val","acc.:{:.1f}".format(acc*100))
+        raise NotImplementedError('%s optimiser has not been implemented!' % args.optimizer)
     
+    optimizer = optimiser_cls([
+                            {'params':model.params1,'weight_decay':args.wd1},
+                            {'params':model.params2,'weight_decay':args.wd2},
+                            ],lr=args.lr)
+    
+    def train():
+        model.train()
+        optimizer.zero_grad()
+        output = model(features,adj)
+        acc_train = accuracy(output[idx_train], labels[idx_train].to(device))
+        loss_train = F.nll_loss(output[idx_train], labels[idx_train].to(device))
+        loss_train.backward()
+        optimizer.step()
+        return loss_train.item(),acc_train.item()
+    
+    
+    def validate():
+        model.eval()
+        with torch.no_grad():
+            output = model(features,adj)
+            loss_val = F.nll_loss(output[idx_val], labels[idx_val].to(device))
+            acc_val = accuracy(output[idx_val], labels[idx_val].to(device))
+            return loss_val.item(),acc_val.item()
+    
+    def test():
+        model.load_state_dict(torch.load(checkpt_file))
+        model.eval()
+        with torch.no_grad():
+            output = model(features, adj)
+            loss_test = F.nll_loss(output[idx_test], labels[idx_test].to(device))
+            acc_test = accuracy(output[idx_test], labels[idx_test].to(device))
+            return loss_test.item(),acc_test.item()
+        
+    t_total = time.time()
+    bad_counter = 0
+    best = 999999999
+    best_epoch = 0
+    acc = 0
+    for epoch in range(args.epochs):
+        loss_tra,acc_tra = train()
+        loss_val,acc_val = validate()
+        if(epoch+1)%1 == 0: 
+            print('Epoch:{:04d}'.format(epoch+1),
+                'train',
+                'loss:{:.3f}'.format(loss_tra),
+                'acc:{:.2f}'.format(acc_tra*100),
+                '| val',
+                'loss:{:.3f}'.format(loss_val),
+                'acc:{:.2f}'.format(acc_val*100))
+        if loss_val < best:
+            best = loss_val
+            best_epoch = epoch
+            acc = acc_val
+            torch.save(model.state_dict(), checkpt_file)
+            bad_counter = 0
+        else:
+            bad_counter += 1
+    
+        if bad_counter == args.patience:
+            break
+    
+    if args.test:
+        acc = test()[1]
+    return acc, t_total, best_epoch
+
+acc_lst = []
+for i in range(0,10):
+    acc, t_total, best_epoch = stat_train()
+    print("Train cost: {:.4f}s".format(time.time() - t_total))
+    print('Load {}th epoch'.format(best_epoch))
+    print("Test" if args.test else "Val","acc.:{:.1f}".format(acc*100))
+    acc_lst.append(acc)
+
+print("Test" if args.test else "Val","acc.:{:.1f}".format(statistics.mean(acc_lst)*100))
 
 
 
